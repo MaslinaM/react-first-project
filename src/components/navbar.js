7766ec366f872668232b4f3954e95e876b091412
MaslinaM <maslina2904001@gmail.com>
2024-11-27 19:37:44 +0530

    first commit

---
+++ src/components/navbar.js
@@ -0,0 +1,11 @@
+export default function Navbar() {
+    return (
+        <header>
+            <nav>
+                <img src="/images/React-icon.png" alt="React logo"></img>
+                <span>ReactFacts</span>
+            </nav>
+        </header>
+        
+    )
+}
